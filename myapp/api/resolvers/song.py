8afeb4229497e03f12e49819a2701165792daf92
George Chokhonelidze <g_chokhonelidze@outlook.com>
2024-09-01 06:29:54 -0500

    09.1.24

--- myapp/api/resolvers/song.py
+++ myapp/api/resolvers/song.py
@@ -3,6 +3,9 @@ from ariadne import convert_kwargs_to_snake_case
 from pytube import YouTube
 import os
 from api import db
+from pytube.innertube import _default_clients
+from pytube import cipher
+import re
 
 @convert_kwargs_to_snake_case
 def list_songs_resolver(obj,info,filters=None):
@@ -52,9 +55,61 @@ def create_song_resolver(obj,info,song):
             "errors":[str(error)]
         }
     return payload
+
+
+_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
+
+def get_throttling_function_name(js: str) -> str:
+    """Extract the name of the function that computes the throttling parameter.
+
+    :param str js:
+        The contents of the base.js asset file.
+    :rtype: str
+    :returns:
+        The name of the function used to compute the throttling parameter.
+    """
+    function_patterns = [
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+    ]
+    #logger.debug('Finding throttling function name')
+    for pattern in function_patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(js)
+        if function_match:
+            #logger.debug("finished regex search, matched: %s", pattern)
+            if len(function_match.groups()) == 1:
+                return function_match.group(1)
+            idx = function_match.group(2)
+            if idx:
+                idx = idx.strip("[]")
+                array = re.search(
+                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
+                        nfunc=re.escape(function_match.group(1))),
+                    js
+                )
+                if array:
+                    array = array.group(1).strip("[]").split(",")
+                    array = [x.strip() for x in array]
+                    return array[int(idx)]
+
+    raise RegexMatchError(
+        caller="get_throttling_function_name", pattern="multiple"
+    )
+
+
+
+
 @convert_kwargs_to_snake_case
 def download_song_resolver(obj,info,link):
     try:
+        cipher.get_throttling_function_name = get_throttling_function_name
         ty = YouTube(str(link))
         video = ty.streams.filter(only_audio=True).first()
         destination = "/downloads/"
@@ -64,7 +119,7 @@ def download_song_resolver(obj,info,link):
         os.rename(out_file, new_file)
         return new_file
     except Exception as error:
-        print(error,flush=True)
+        print("error=",error,flush=True)
 
 @convert_kwargs_to_snake_case
 def update_song_resolver(obj,info,song):
